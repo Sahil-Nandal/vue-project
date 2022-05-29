6dd546a933e8cbd5f7ad1bc3665a7b46b9228c88
Sahil-Nandal <sahil52001nandal@gmail.com>
2022-05-29 20:04:11 +0530

    1 file modified

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,23 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
 
 defineProps({
-  msg: String
-})
+  msg: String,
+});
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
+  <h2>This project is all about learning!</h2>
 </template>
 
 <style scoped>
 a {
   color: #42b983;
+}
+h2 {
+  background-color: yellow;
 }
 </style>
